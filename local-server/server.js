82cbcb5abccacb2ba577ee358c2a2703e7816fa3
Hai Nam <hainam@kaist.ac.kr>
2022-08-06 12:37:00 +0900

    minor rename

--- local-server/server.js
+++ local-server/server.js
@@ -1,13 +1,15 @@
+const NUM_PORT = 8081
+
 let express = require("express");
 let app = express();
 
 app.use(function(req, res, next){
-    console.log(`${new Date()} - ${req.method} reqest for ${req.url}`);
+    console.log(`${new Date()} - ${req.method} request for ${req.url}`);
     next();
 });
 
 app.use(express.static("../static"));
 
-app.listen(8081, function(){
-    console.log("Serving at 8081")
+app.listen(NUM_PORT, function(){
+    console.log(`Serving at ${NUM_PORT}`)
 });
