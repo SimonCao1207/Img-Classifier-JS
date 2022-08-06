60e20bb33ab71165cb0d567c0042e9c4611b9511
Hai Nam <hainam@kaist.ac.kr>
2022-08-06 13:30:32 +0900

    load model, wip: predict image with base model

--- static/predict.js
+++ static/predict.js
@@ -17,5 +17,34 @@ $('#image-selector').change(function(){
   reader.readAsDataURL(file)
 })
 
+// Select model
+$("#model-selector").change(function(){
+  loadModel($("#model-selector").val())
+  $('.progress-bar').show()
+})
+
+let model
+// Load pretrained model
+async function loadModel(modelName){
+  model = await tf.loadLayersModel(`http://localhost:8081/model/${modelName}/model.json`)
+  $('.progress-bar').hide()
+}
+
+// TODO: Load pretrained models from https://github.com/tensorflow/tfjs-models
 
 
+$("#predict-button").click(async function(){
+  let image = $("selected-image").get(0)
+  console.log(image);
+  // let tensor = tf.browser.fromPixels(image).toFloat()
+  // tensor.print()
+  // let tensor = preprocessImage(image, $("#model-selector").val)
+  // let prediction = await model.predict(tensor).data()
+  // console.log("prediction", prediction);
+})
+
+function preprocessImage(image, modelName){
+  // TODO: Preprocess Image before input into model
+  let tensor = tf.browser.fromPixels(image).toFloat()
+  return tensor.expandDims();
+}
