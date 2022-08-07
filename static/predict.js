de62f9d550cd2c9c3560870af24c57d69fd03945
Hai Nam <hainampbc@gmail.com>
2022-08-07 13:45:35 +0900

    add model mobile net

--- static/predict.js
+++ static/predict.js
@@ -19,6 +19,7 @@ $('#image-selector').change(function(){
 
 // Select model
 $("#model-selector").change(function(){
+  console.log("Loading the model: ", $("#model-selector").val())
   loadModel($("#model-selector").val())
   $('.progress-bar').show()
 })
@@ -26,7 +27,11 @@ $("#model-selector").change(function(){
 let model
 // Load pretrained model
 async function loadModel(modelName){
-  model = await tf.loadLayersModel(`http://localhost:8081/model/${modelName}/model.json`)
+  if (modelName == "MobileNet"){
+    const modelUrl = 'https://tfhub.dev/google/imagenet/mobilenet_v2_140_224/classification/2';
+    model = await tf.loadGraphModel(modelUrl, {fromTFHub: true})
+  }
+  else model = await tf.loadLayersModel(`http://localhost:8081/model/${modelName}/model.json`)
   $('.progress-bar').hide()
 }
 
@@ -36,15 +41,46 @@ async function loadModel(modelName){
 $("#predict-button").click(async function(){
   let image = $("#selected-image").get(0)
   console.log("image". image);
-  let tensor = preprocessImage(image, $("#model-selector").val)
+  let tensor = preprocessImage(image, $("#model-selector").val())
   let prediction = await model.predict(tensor).data()
-  console.log("prediction", prediction);
+  let top5 = Array.from(prediction).map(function(p,i){
+      return {
+        probability: p,
+        className: IMAGENET_CLASSES[i]
+      }
+    }).sort(function(a, b){ 
+      return b.probability - a.probability
+    }).slice(0, 5)
+
+    $("#prediction-list").empty()
+  top5.forEach(function(pred){
+    $("#prediction-list").append(`<li>${pred.className}:${pred.probability.toFixed(6)}</li>`)
+  })
 })
 
+
 function preprocessImage(image, modelName){
-  // TODO: Preprocess Image before input into model
+  
   let tensor = tf.browser.fromPixels(image)
       .resizeNearestNeighbor([224,224])
       .toFloat()
-  return tensor.expandDims();
+
+  if (modelName == "VGG16"){
+    let meanImageNetRGB = tf.tensor1d([123.68, 116.779, 103.939])
+    return tensor.sub(meanImageNetRGB)
+                .reverse(2)
+                .expandDims()
+  }
+
+  else if (modelName == "MobileNet"){
+    let offset = tf.scalar(127.5)
+    return tensor.sub(offset)
+                 .div(offset)
+                 .expandDims()
+  }
+
+  else {
+  // TODO: Preprocess Image before input into model
+    return tensor.expandDims()
+  }
 }
